b3f76a2710d81c9ce1e2294727bda2717414ede9
Turtilla <98593377+Turtilla@users.noreply.github.com>
2022-10-16 12:39:33 +0200

    Initializing the project
    -updated index
    -updated requirements
    -updated http-service (not tested)

---
+++ README.md
@@ -0,0 +1,4 @@
+# Dialogue Systems 2 - final project "Shelley"
+The final project by Maria Irena Szawerna for the Dialogue Systems 2 course in the Master in Language Technology of the University of Gothenburg.
+
+This dialogue system provides instructions on how to perform daily and weekly tortoise care, and can provide information on what is edible for these creatures using custom "API"-like functions to retrieve information from (The Tortoise Table)[https://www.thetortoisetable.org.uk/]. 
---
+++ ddds/http-service/http_service.py
@@ -0,0 +1,178 @@
+# -*- coding: utf-8 -*-
+
+import json
+
+from flask import Flask, request
+from jinja2 import Environment
+
+import TTT_API as ttt
+
+app = Flask(__name__)
+environment = Environment()
+
+
+def jsonfilter(value):
+    return json.dumps(value)
+
+
+environment.filters["json"] = jsonfilter
+
+
+def error_response(message):
+    response_template = environment.from_string("""
+    {
+      "status": "error",
+      "message": {{message|json}},
+      "data": {
+        "version": "1.0"
+      }
+    }
+    """)
+    payload = response_template.render(message=message)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def query_response(value, grammar_entry):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.1",
+        "result": [
+          {
+            "value": {{value|json}},
+            "confidence": 1.0,
+            "grammar_entry": {{grammar_entry|json}}
+          }
+        ]
+      }
+    }
+    """)
+    payload = response_template.render(value=value, grammar_entry=grammar_entry)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def multiple_query_response(results):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.0",
+        "result": [
+        {% for result in results %}
+          {
+            "value": {{result.value|json}},
+            "confidence": 1.0,
+            "grammar_entry": {{result.grammar_entry|json}}
+          }{{"," if not loop.last}}
+        {% endfor %}
+        ]
+      }
+    }
+     """)
+    payload = response_template.render(results=results)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+def validator_response(is_valid):
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.0",
+        "is_valid": {{is_valid|json}}
+      }
+    }
+    """)
+    payload = response_template.render(is_valid=is_valid)
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route("/dummy_query_response", methods=['POST'])
+def dummy_query_response():
+    response_template = environment.from_string("""
+    {
+      "status": "success",
+      "data": {
+        "version": "1.1",
+        "result": [
+          {
+            "value": "dummy",
+            "confidence": 1.0,
+            "grammar_entry": null
+          }
+        ]
+      }
+    }
+     """)
+    payload = response_template.render()
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
+@app.route("/action_success_response", methods=['POST'])
+def action_success_response():
+    response_template = environment.from_string("""
+   {
+     "status": "success",
+     "data": {
+       "version": "1.1"
+     }
+   }
+   """)
+    payload = response_template.render()
+    response = app.response_class(
+        response=payload,
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+@app.route("/edibility")
+def edibility():
+    payload = request.get_json()
+    # exctracting the plant name
+    plant = payload["request"]["parameters"]["wh_plant"]["grammar_entry"].lower()
+
+    print(plant)  # testing
+
+    # making the request
+    plant_dict = ttt.get_plant_dict(plant) 
+
+    # retrieving the information from the dict
+    if len(plant_dict) == 0:
+      # if there are no results in the search
+      return query_response(value='I have no information about such a plant. Just to be safe, please do not feed it to the tortoise.', grammar_entry=None)
+    elif plant in plant_dict:
+      # if the plant name is present in the dictionary
+      answer = plant_dict[plant]['edibility']
+      return query_response(value=f'{plant} has the status {answer}', grammar_entry=None)
+    else:
+      # if there are results but none of them match 1:1; the first one is then selected (as that one is often the best match, I am unsure how the website's search works)
+      plant_guess = next(iter(plant_dict.items()))[0]
+      answer = plant_dict[plant_guess]['edibility']
+      return query_response(value=f'I found {plant_guess}. It has the status {answer}. If you meant another plant, try giving me its full name or latin name.', grammar_entry=None)
