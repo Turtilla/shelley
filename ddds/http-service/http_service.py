4de063bb85962da0985138e1fa39e8901f8ed530
Turtilla <98593377+Turtilla@users.noreply.github.com>
2022-10-19 19:51:59 +0200

    Update http_service.py
    -removing the importing of an external script as that was causing errors
    -adding a function from TTT_API.py
    -adding methods=['POST']

--- ddds/http-service/http_service.py
+++ ddds/http-service/http_service.py
@@ -4,8 +4,8 @@ import json
 
 from flask import Flask, request
 from jinja2 import Environment
-
-import TTT_API as ttt
+from bs4 import BeautifulSoup
+import requests
 
 app = Flask(__name__)
 environment = Environment()
@@ -152,16 +152,14 @@ def action_success_response():
     )
     return response
 
-@app.route("/edibility")
+@app.route("/edibility", methods=['POST'])
 def edibility():
     payload = request.get_json()
     # exctracting the plant name
     plant = payload["request"]["parameters"]["wh_plant"]["grammar_entry"].lower()
 
-    print(plant)  # testing
-
     # making the request
-    plant_dict = ttt.get_plant_dict(plant) 
+    plant_dict = get_plant_dict(plant) 
 
     # retrieving the information from the dict
     if len(plant_dict) == 0:
@@ -176,3 +174,38 @@ def edibility():
       plant_guess = next(iter(plant_dict.items()))[0]
       answer = plant_dict[plant_guess]['edibility']
       return query_response(value=f'I found {plant_guess}. It has the status {answer}. If you meant another plant, try giving me its full name or latin name.', grammar_entry=None)
+
+# I was getting errors when importing my own script so I copied the relevant function here
+def get_plant_dict(plant_name):
+    # a function intended for retrieving information about plants matching
+    # the user request in a dictionary format name:edibility
+    
+    # the url/website from which the results are sources
+    website = requests.get(url=f"https://www.thetortoisetable.org.uk/plant-database/viewplants/search/?searchtogglestatus=&searchchoice=exactwords&searchtxt={plant_name}&x=0&y=0#.YxI5UXZBzD4").text
+    
+    # turning the website into a BeautifulSoup object
+    soup = BeautifulSoup(website, 'html.parser')  
+    # retrieving a list of text elements only pertaining to plants
+    plant_contents = list(soup.find(id='content').stripped_strings)[3:]
+    
+    # creating a list of lists, where every element is one plant
+    all_plants = []
+    temporary_plant = []
+    # the elements can be edibility, plant name, alternative names, Latin
+    # names, and 'See More'
+    for element in plant_contents:
+        if element != 'See More':  # 'See More' delineates separate plants
+            temporary_plant.append(element)
+        else:  # if it is 'See More'
+            all_plants.append(temporary_plant)
+            temporary_plant = []
+    
+    # creating the dictionary to be returned
+    plant_dict = {}
+    for entry in all_plants:
+        edibility = entry[0].lower()
+        name = entry[1].lower()
+        latin = entry[-1].lower()  # it is always the last entry, but some plants have alternative names as [2] and some do not
+        plant_dict[name] = {"edibility":edibility, "latin":latin}
+        
+    return plant_dict
